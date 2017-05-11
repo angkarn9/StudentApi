710cf60bc98b47c92e89774a9f32aaf9bcacc4bb
angkarn <angkarn9@gmail.com>
2017-05-12 00:15:00 +0700

    call stub data

--- src/api.go
+++ src/api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"sync"
@@ -12,11 +13,33 @@ type Routing struct {
 	sync.RWMutex
 }
 
-func (rt *Routing) Index(w rest.ResponseWriter, r *rest.Request) {
-	w.WriteJson(map[string]string{
-		"id":   "1",
-		"name": "Golang",
-	})
+type Students struct {
+	StudentList []Student `json:"studentList"`
+}
+
+type Student struct {
+	ID                   string `json:"id"`
+	Name                 string `json:"name"`
+	LastName             string `json:"lastName"`
+	BasicEnglish1        string `json:"basicEnglish1"`
+	Calculus1            string `json:"calculus1"`
+	ComputerGraphics     string `json:"computerGraphics"`
+	SoftwareArchitecture string `json:"softwareArchitecture"`
+	DatabaseSystem       string `json:"databaseSystem"`
+}
+
+func (rt *Routing) GetStudentInfo(w rest.ResponseWriter, r *rest.Request) {
+	url := "http://localhost:8882/api/student"
+	response, err := http.Get(url)
+	if err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	defer response.Body.Close()
+
+	studentList := Students{}
+
+	json.NewDecoder(response.Body).Decode(&studentList)
+	w.WriteJson(studentList)
 }
 
 func main() {
@@ -25,7 +48,7 @@ func main() {
 	api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
 	router, err := rest.MakeRouter(
-		rest.Get("/", routing.Index),
+		rest.Get("/api/student", routing.GetStudentInfo),
 	)
 	if err != nil {
 		log.Fatal(err)
--- src/api_test.go
+++ src/api_test.go
@@ -9,7 +9,7 @@ func TestCallIndex(t *testing.T) {
 	routing := Routing{}
 	testApi := testsimpleapi.TestSimpleApi{
 		Url:    "/",
-		Func:   routing.Index,
+		Func:   routing.GetStudentInfo,
 		Method: "GET",
 		ReqUrl: "/",
 		Header: map[string]string{},
@@ -19,7 +19,27 @@ func TestCallIndex(t *testing.T) {
 	recorded.CodeIs(200)
 	recorded.ContentTypeIsJson()
 	recorded.BodyIs(`{
-  "id": "1",
-  "name": "Golang"
+  "studentList": [
+    {
+      "id": "6004101330",
+      "name": "phatcharaphan",
+      "lastName": "ananpreechakun",
+      "basicEnglish1": "3.5",
+      "calculus1": "4.0",
+      "computerGraphics": "2.5",
+      "softwareArchitecture": "1.0",
+      "databaseSystem": "3.0"
+    },
+    {
+      "id": "6004101332",
+      "name": "angkarn",
+      "lastName": "janjuang",
+      "basicEnglish1": "4.0",
+      "calculus1": "3.0",
+      "computerGraphics": "3.5",
+      "softwareArchitecture": "2.0",
+      "databaseSystem": "3.5"
+    }
+  ]
 }`)
 }
